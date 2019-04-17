091bf71c4f57f00159eb7dd02e684475de0f0ed1
Zhiw <karlkb81@gmail.com>
2019-04-17 14:12:56 +0800

    feat(business_rule): sync business_rule to stream

--- server/app/services/rules/views/business_rules.py
+++ server/app/services/rules/views/business_rules.py
@@ -1,17 +1,20 @@
-from flask import jsonify, request
+from typing import AnyStr
+
+from flask import jsonify, request, current_app
 from sqlalchemy.exc import IntegrityError
 
 from actor_libs.database.orm import db
 from actor_libs.errors import (
-    ReferencedError, FormInvalid
+    ReferencedError, FormInvalid, InternalError
 )
+from actor_libs.http_tools.sync_http import SyncHttp
 from actor_libs.utils import get_delete_ids
 from app import auth
 from app.models import (
     User, BusinessRule, Action, BusinessRuleAction
 )
 from . import bp
-from ..schemas import BusinessRuleSchema
+from ..schemas import BusinessRuleSchema, AlertActionSchema, UpdateBusinessRuleSchema
 
 
 @bp.route('/business_rules')
@@ -19,7 +22,7 @@ from ..schemas import BusinessRuleSchema
 def list_business_rules():
     query = BusinessRule.query \
         .with_entities(BusinessRule.id, BusinessRule.ruleName, BusinessRule.sql,
-                       BusinessRule.enable)
+                       BusinessRule.enable, BusinessRule.remark)
 
     records = query.pagination()
     return jsonify(records)
@@ -41,6 +44,9 @@ def create_business_rule():
     for action in actions:
         new_rule.actions.append(action)
 
+    rule_json = get_rule_json(new_rule)
+    url = f"{current_app.config.get('STREAM_RULE_URL')}/"
+    stream_rule_http('post', url=url, json=rule_json)
     db.session.commit()
     record = new_rule.to_dict()
     return jsonify(record), 201
@@ -70,15 +76,24 @@ def update_business_rule(rule_id):
     business_rule = BusinessRule.query \
         .filter(BusinessRule.id == rule_id) \
         .first_or_404()
-    request_dict = BusinessRuleSchema.validate_request(obj=business_rule)
+    old_enable = business_rule.enable
+    request_dict = UpdateBusinessRuleSchema.validate_request(obj=business_rule)
     updated_business_rule = business_rule.update(request_dict, commit=False)
 
     action_ids = request.get_json().get('actions')
-    update_actions = Action.query \
-        .filter(Action.id.in_(action_ids)) \
-        .many()
-    updated_business_rule.actions = update_actions
-
+    if action_ids:
+        update_actions = Action.query \
+            .filter(Action.id.in_(action_ids)) \
+            .many()
+        updated_business_rule.actions = update_actions
+    rule_json = get_rule_json(updated_business_rule)
+    changed = old_enable != updated_business_rule.enable
+    url = f"{current_app.config.get('STREAM_RULE_URL')}/{rule_id}"
+    stream_rule_http('put', url=url, json=rule_json)
+    if changed:
+        switch = 'start' if updated_business_rule.enable else 'stop'
+        switch_url = f"{current_app.config.get('STREAM_RULE_URL')}/{rule_id}/{switch}"
+        stream_rule_http('put', url=switch_url)
     db.session.commit()
     record = updated_business_rule.to_dict()
     return jsonify(record)
@@ -94,7 +109,42 @@ def delete_business_rule():
             .many()
         for rule in rules:
             db.session.delete(rule)
+            url = f"{current_app.config.get('STREAM_RULE_URL')}/{rule.id}"
+            stream_rule_http('delete', url=url)
         db.session.commit()
     except IntegrityError:
         raise ReferencedError()
     return '', 204
+
+
+def get_rule_json(business_rule):
+    rule_actions = []
+    for action in business_rule.actions:
+        if action.actionType == 1:
+            alert_dict = AlertActionSchema().dump(action.config).data
+            action_config = {
+                'webhook': {
+                    **alert_dict,
+                    'url': current_app.config.get('CURRENT_ALERT_URL'),
+                    'ruleIntID': business_rule.id
+
+                }
+            }
+            rule_actions.append(action_config)
+    rule_json = {
+        'id': business_rule.id,
+        'sql': business_rule.sql,
+        'enabled': True if business_rule.enable == 1 else False,
+        'actions': rule_actions
+    }
+    return rule_json
+
+
+def stream_rule_http(method: AnyStr, **kwargs):
+    with SyncHttp() as sync_http:
+        response = getattr(sync_http, method)(**kwargs)
+
+    if response.responseCode not in [200, 201]:
+        db.session.rollback()
+        raise InternalError(field='stream')
+    return response
