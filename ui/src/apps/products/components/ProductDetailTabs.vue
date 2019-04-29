8e4449c2a79456bcef303ed07c88b3dc60c0c76c
ysfscream <894402575@qq.com>
2019-04-29 10:29:55 +0800

    refactor(products): remove LwM2M product items

--- ui/src/apps/products/components/ProductDetailTabs.vue
+++ ui/src/apps/products/components/ProductDetailTabs.vue
@@ -23,21 +23,13 @@ export default {
       return this.$store.state.products.currentProducts
     },
     tabs() {
-      const LWM2M = 3
       const { id } = this.$route.params
-      const currentProducts = JSON.parse(localStorage.getItem('currentProducts')) || []
-      const currentProduct = currentProducts.find(
-        item => item.productIntID === parseInt(id, 10),
-      )
       const data = [
         { code: 'product_info', url: `/products/${id}` },
         { code: 'devices', url: `/products/${id}/devices` },
         { code: 'definition', url: `/products/${id}/definition` },
         { code: 'codec', url: `/products/${id}/codec` },
       ]
-      if (currentProduct.cloudProtocol === LWM2M) {
-        data.splice(4, 0, { code: 'items', url: `/products/${id}/items` })
-      }
       return data
     },
   },
--- ui/src/apps/products/routes.js
+++ ui/src/apps/products/routes.js
@@ -2,7 +2,6 @@ const routes = [
   { path: 'products', component: () => import('./views/Products') },
   { path: 'products/:id', component: () => import('./views/ProductDetails') },
   { path: 'products/:id/devices', component: () => import('./views/ProductDevices') },
-  { path: 'products/:id/items', component: () => import('./views/ProductItems') },
   { path: 'products/:id/definition', component: () => import('./views/ProductDefinition') },
   { path: 'products/:id/definition/:streamID', component: () => import('./views/DataStreamDetails') },
   { path: 'products/:id/codec', component: () => import('./views/ProductCodec.vue') },
