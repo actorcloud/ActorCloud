e5234f1c411b98cd842efa93b1e4e8871b50d118
Zhiw <karlkb81@gmail.com>
2019-05-07 16:38:56 +0800

    feat(last_event): add api to get device's last event

--- server/app/services/device_data/views/events.py
+++ server/app/services/device_data/views/events.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from sqlalchemy import text, desc
 
 from app import auth
 from app.models import Client, DeviceEvent
@@ -19,3 +20,21 @@ def list_client_events(client_id):
     records = events_query.pagination(code_list=['dataType'])
 
     return jsonify(records)
+
+
+@bp.route('/devices/<int:client_id>/last_event')
+@auth.login_required
+def view_client_last_event(client_id):
+    client = Client.query \
+        .with_entities(Client.deviceID, Client.tenantID) \
+        .filter(Client.id == client_id).first_or_404()
+
+    event = DeviceEvent.query \
+        .filter_tenant(tenant_uid=client.tenantID) \
+        .filter(DeviceEvent.deviceID == client.deviceID) \
+        .filter(DeviceEvent.msgTime >= text("NOW() - INTERVAL '1 DAYS'")) \
+        .order_by(desc(DeviceEvent.msgTime)) \
+        .first()
+
+    record = event.to_dict(code_list=['dataType']) if event else {}
+    return jsonify(record)
