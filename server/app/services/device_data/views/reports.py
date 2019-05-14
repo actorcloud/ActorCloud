12b3471aca65bd8314023163d2080183511f702e
Mousse <zibuyu1995@gmail.com>
2019-05-14 09:23:29 +0800

    feat(reports): reports api filtering app permissions

--- server/app/services/device_data/views/reports.py
+++ server/app/services/device_data/views/reports.py
@@ -11,7 +11,7 @@ from ._reports_type import REPORTS_TYPE_FUNC
 @auth.login_required(permission_required=False)
 def reports_data():
     request_args = _validate_request_args()
-    report_type = request_args['reportType']
+    report_type = request_args.get('reportType')
     reports_data_func = REPORTS_TYPE_FUNC.get(report_type)
     if not reports_data_func:
         raise ParameterInvalid(field='reportsType')
