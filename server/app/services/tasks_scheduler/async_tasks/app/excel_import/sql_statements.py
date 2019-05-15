6a1200153a64e9f8bb7d45915d19cfc777cf34ab
Zhiw <karlkb81@gmail.com>
2019-05-15 09:59:57 +0800

    refactor(excel): refactor devices import and export

--- server/app/services/tasks_scheduler/async_tasks/app/excel_import/sql_statements.py
+++ server/app/services/tasks_scheduler/async_tasks/app/excel_import/sql_statements.py
@@ -44,7 +44,7 @@ WHERE "deviceID" = ANY ('{{{devices_uid}}}'::varchar[])
 query_device_by_username_sql = """
 SELECT "deviceID", "deviceUsername"
 FROM devices
-WHERE ("deviceID","deviceUsername") IN {devices}
+WHERE ("deviceID","deviceUsername") IN ({devices})
 """
 
 query_gateway_sql = """
@@ -55,32 +55,24 @@ WHERE devices."deviceName" = ANY ('{{{devices_name}}}'::varchar[])
     AND devices."tenantID" = '{tenantID}';
 """
 
-query_sub_sql = """
-SELECT "productID", product_sub.topic, product_sub.qos
-FROM products
-       JOIN product_sub ON product_sub."productIntID" = products.id
-WHERE products."productID" = ANY ('{{{products_uid}}}'::varchar[]);
-"""
-
 insert_device_sql = """
 WITH {client} AS (
     INSERT INTO devices("createAt", "updateAt", "deviceID", "deviceName", "softVersion",
         "hardwareVersion", manufacturer, "serialNumber", "location", longitude, latitude,
         "deviceUsername", token, "authType", "deviceStatus", "deviceConsoleIP",
-        "deviceConsoleUsername", "deviceConsolePort", "upLinkSystem", "IMEI", "IMSI", carrier,
-        "physicalNetwork", blocked, "autoSub", description, mac, type, "productID",
+        "deviceConsoleUsername", "deviceConsolePort", "upLinkNetwork", carrier,
+        blocked, description, mac, "deviceType", "productID",
         "userIntID", "tenantID")
     VALUES ('{createAt}', '{updateAt}', '{deviceID}', '{deviceName}', '{softVersion}',
         '{hardwareVersion}', '{manufacturer}', '{serialNumber}', '{location}', {longitude},
         {latitude}, '{deviceUsername}', '{token}', {authType}, {deviceStatus},
         '{deviceConsoleIP}', '{deviceConsoleUsername}',{deviceConsolePort},
-        {upLinkSystem}, '{IMEI}', '{IMSI}',{carrier}, {physicalNetwork}, {blocked},
-        {autoSub}, '{description}', '{mac}', '{type}', '{productID}', {userIntID},
-        '{tenantID}')
+        {upLinkNetwork}, {carrier}, {blocked},'{description}', '{mac}', {deviceType}, '{productID}',
+         {userIntID}, '{tenantID}')
     RETURNING id
 )
-INSERT INTO devices(id, gateway, lora, "modBusIndex", "metaData", "parentDevice")
-    SELECT id, {gateway}, {lora}, {modBusIndex}, {metaData}, {parentDevice}
+INSERT INTO end_devices(id, "upLinkSystem", gateway, "loraData", "lwm2mData", "modbusData", "parentDevice")
+    SELECT id, {upLinkSystem}, {gateway}, '{loraData}', '{lwm2mData}', '{modbusData}', {parentDevice}
     FROM {client};
 """
 
@@ -88,19 +80,9 @@ query_column_default = """
 SELECT column_name, column_default
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE table_name = 'devices'
-   OR table_name = 'devices';
+   OR table_name = 'end_devices';
 """
 
-insert_sub_sql = """
-INSERT INTO mqtt_sub("createAt", topic, qos, "clientID","deviceIntID")
-SELECT '{createAt}', '{topic}', {qos}, '{clientID}', {deviceIntID}
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM mqtt_sub
-    WHERE "clientID"='{clientID}'
-        AND "deviceIntID"={deviceIntID}
-);
-"""
 query_device_by_product_sql = """
 SELECT id, "productID", CONCAT_WS(':',"tenantID","productID", "deviceID") AS client_id
 FROM devices
@@ -108,8 +90,9 @@ WHERE "productID" = ANY ('{{{products_uid}}}'::varchar[]);
 """
 
 query_device_by_imei_sql = """
-SELECT "IMEI"
-FROM devices
-WHERE "IMEI" = ANY ('{{{devices_imei}}}'::varchar[])
+SELECT end_devices."lwm2mData"::jsonb ->>'IMEI' as "IMEI"
+FROM end_devices
+    JOIN devices ON devices.id=end_devices.id
+WHERE  end_devices."lwm2mData"::jsonb ->>'IMEI' = ANY ('{{{devices_imei}}}'::varchar[])
     AND "tenantID"='{tenantID}';
 """
