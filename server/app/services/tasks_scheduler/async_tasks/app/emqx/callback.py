3da42e3070eb3b84b2d0aefcc91c125e6aaa3b89
Mousse <zibuyu1995@gmail.com>
2019-05-31 16:27:48 +0800

    feat(emqx): move device auth and callback to async tasks

---
+++ server/app/services/tasks_scheduler/async_tasks/app/emqx/callback.py
@@ -0,0 +1,110 @@
+import json
+import logging
+from datetime import datetime
+
+from actor_libs.database.async_db import db
+from actor_libs.http_tools.async_http import AsyncHttp
+from .sql_statements import (
+    query_base_devices_sql, insert_connect_logs_sql,
+    update_publish_logs_sql
+)
+from ..config import project_config
+from ..extra import HttpException
+
+
+__all__ = ['backend_callback']
+
+
+logger = logging.getLogger(__name__)
+
+
+async def backend_callback(request_dict):
+    request_dict['callback_date'] = datetime.now()
+    callback_action = request_dict.get('action')
+    handle_action_funcs = {
+        'client_connected': client_connected_callback,
+        'client_disconnected': client_disconnected_callback,
+        'message_acked': message_acked_callback,
+    }
+    if not handle_action_funcs.get(callback_action):
+        raise HttpException(code=404)
+    handle_action = handle_action_funcs[callback_action]
+    await handle_action(request_dict)
+    return {'status': 200}, 200
+
+
+async def client_disconnected_callback(request_dict) -> None:
+    device_info = await _query_device_info(
+        request_dict.get('client_id'),
+        request_dict.get('username'),
+    )
+    connect_dict = {
+        'msgTime': request_dict['callback_date'],
+        'deviceID': device_info['deviceID'],
+        'tenantID': device_info['tenantID'],
+        'connectStatus': 0,
+        'IP': None
+    }
+    await db.execute(insert_connect_logs_sql.format(**connect_dict))
+
+
+async def client_connected_callback(request_dict) -> None:
+    """ Device connected subscribe inbox topic """
+
+    device_info = await _query_device_info(
+        request_dict.get('client_id'),
+        request_dict.get('username'),
+    )
+    if device_info['protocol'] == 'lwm2m':
+        # if device protocol is lwm2m pass
+        return
+
+    auto_sub_topic = (
+        f"/{device_info['protocol']}/{device_info['tenantID']}"
+        f"/{device_info['productID']}/{device_info['deviceID']}/inbox"
+    )
+    request_json = {
+        'topic': auto_sub_topic,
+        'qos': 1,
+        'client_id': device_info['deviceID']
+    }
+    emqx_sub_url = f"{project_config['EMQX_API']}/mqtt/subscribe"
+    async with AsyncHttp(auth=project_config['EMQX_AUTH']) as async_http:
+        response = await async_http.post_url(
+            url=emqx_sub_url, json=request_json
+        )
+        logger.info(response)
+
+
+async def message_acked_callback(request_dict) -> None:
+    """ Update the publish status when the device receives the publish message """
+
+    device_id = request_dict.get('client_id')
+    payload = request_dict.get('payload')
+    if not device_id or not payload:
+        raise HttpException(code=404)
+    try:
+        load_payload = json.loads(payload)
+    except Exception:
+        raise HttpException(code=404)
+    task_id = load_payload.get('task_id')
+    if not task_id:
+        raise HttpException(code=404)
+
+    await db.execute(
+        update_publish_logs_sql.format(publishStatus=2, taskID=task_id)
+    )
+
+
+async def _query_device_info(device_id, device_username):
+    if not device_id or not device_username:
+        raise HttpException(code=404, field='devices')
+    filter_username_sql = """ AND devices."deviceUsername" = '{deviceUsername}' """
+    device_query_sql = query_base_devices_sql + filter_username_sql
+    query_result = await db.fetch_row(
+        device_query_sql.format(deviceID=device_id, deviceUsername=device_username)
+    )
+    if not query_result:
+        raise HttpException(404, field='device')
+    device_info = dict(query_result)
+    return device_info
--- server/app/services/tasks_scheduler/async_tasks/app/emqx/sql_statements.py
+++ server/app/services/tasks_scheduler/async_tasks/app/emqx/sql_statements.py
@@ -1,3 +1,51 @@
+query_base_devices_sql = """
+SELECT
+       devices.id, devices."authType", devices."deviceID", 
+       devices."deviceUsername", devices.token,
+       devices."productID", devices."tenantID",
+       lower(dict_code."enLabel") AS protocol
+FROM devices
+JOIN products  ON devices."productID" = products."productID"
+JOIN dict_code ON dict_code."codeValue" = products."cloudProtocol"
+WHERE
+      devices."deviceID" = '{deviceID}'
+  AND devices.blocked = 0
+  AND dict_code.code = 'cloudProtocol'
+"""
+
+device_cert_auth_sql = """
+SELECT
+       devices.id, devices."authType", devices."deviceID", 
+       devices."deviceUsername", devices.token,
+       lower(dict_code."enLabel") AS protocol
+FROM devices
+JOIN certs_devices ON certs_devices."deviceIntID" = devices.id
+JOIN certs ON certs.id = certs_devices."certIntID"
+JOIN products  ON devices."productID" = products."productID"
+JOIN dict_code ON dict_code."codeValue" = products."cloudProtocol"
+WHERE
+      devices."deviceID" = '{deviceID}'
+  AND devices.blocked = 0
+  AND certs."CN" = '{CN}'
+  AND certs.enable = 1
+  AND dict_code.code = 'cloudProtocol'
+"""
+
+insert_connect_logs_sql = """
+INSERT INTO "connect_logs"
+    ("IP", "connectStatus", "msgTime", "deviceID", "tenantID") 
+VALUES('{IP}', {connectStatus}, '{msgTime}', '{deviceID}', '{tenantID}') 
+"""
+
+update_device_sql = """
+UPDATE "devices" 
+SET 
+    "lastConnection"='{lastConnection}', 
+    "deviceStatus"={deviceStatus} 
+WHERE 
+     devices."deviceID" = '{deviceID}'
+"""
+
 insert_publish_logs_sql = """
 INSERT INTO "publish_logs"
     ("topic", "streamID", "payload", "publishStatus", 
@@ -7,7 +55,7 @@ VALUES ('{topic}', '{streamID}', '{payload}', '1', '{taskID}',
 """
 
 update_publish_logs_sql = """
-UPDATE client_publish_logs
-SET "publishStatus"={taskStatus}
-WHERE client_publish_logs."taskID"='{taskID}'
+UPDATE publish_logs
+SET "publishStatus"={publishStatus}
+WHERE publish_logs."taskID"='{taskID}'
 """
