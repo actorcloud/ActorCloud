313bb3d462a9229e9ed7f749d84bcf86d7c9019f
Mousse <zibuyu1995@gmail.com>
2019-06-13 10:22:07 +0800

    feat(run): add click command

--- server/runserver.py
+++
@@ -1,5 +0,0 @@
-
-from manage import app
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7000, debug=True)
--- server/runserver.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-FLASK_DEBUG=True
-export FLASK_ENV=development
-flask run -h 0.0.0.0 -p 7000
